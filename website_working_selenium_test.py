2d8996600d2e3ac67fbf81995047841ffc75c9f8
Kms2229 <kms22@bu.edu>
2022-12-26 16:23:50 +0530

    edits

--- website_working_selenium_test.py
+++ website_working_selenium_test.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.keys import Keys
 
 class ChromeSearch(unittest.TestCase):
     def test_website_working(self):
-        driver = webdriver.Chrome(executable_path='C:/Users/shahk/chromedriver_win32/chromedriver.exe')
-        driver.get("https://bu-marketplace.herokuapp.com/log_in")
-        self.assertIn("BU MarketPlace", driver.title)
+        driver = webdriver.Chrome(executable_path=' ')
+        driver.get(" ")
+        self.assertIn(" ", driver.title)
         
     
 if __name__ == "__main__":
